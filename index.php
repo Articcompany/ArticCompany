936e3a7d9c5cad142dee0666558ab0df99e2c7a4
cealmaster <v_ani_ivan@hotmail.com>
2018-03-23 08:56:14 -0500

    Primera version de pagina web de Artic Company S.A.S.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Artic Company S.A.S.</title>
+    <?php include './partes/head.php'; ?>
+</head>
+
+<body id="pagina-principal">
+    <?php 
+        include './partes/header.php';
+        include './partes/servicios.php';
+        include './partes/portafolio.php';
+        include './partes/modalesportafolio.php';
+        include './partes/equipo.php';
+        include './partes/contactanos.php';
+        include './partes/footer.php';
+        include './partes/scripts.php'
+    ?>
+    
+</body>
+
+
+</html>
---
+++ partes/header.php
@@ -0,0 +1,34 @@
+<header class="masthead">
+        <div class="container">
+            <div class="intro-text wow fadeInUp">
+                <div class="intro-lead-in  ">Soluciones de software</div>
+                <div class="intro-heading text-uppercase  ">Desarrollo Limpio</div>
+                <a class="oli btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#Servicios">Conocenos</a>
+            </div>
+        </div>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#pagina-principal"><img class="logo" src="img/LogoLetraDerecha.png" alt="logo artic company"></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsivo" aria-controls="navbarResponsivo" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fa fa-bars"></i>
+                    </button>
+            <div class="collapse navbar-collapse" id="navbarResponsivo">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#Servicios">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#portafolio">portafolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#equipo">equipo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#contactanos">contactanos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
