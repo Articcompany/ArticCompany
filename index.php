8546890e60ad9a8d80a6b21d5ede77d22ecca9bf
kevkray <kevindiaz9@outlook.com>
2018-05-02 16:05:32 -0500

    primer cambio Kevin

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Artic Company S.A.S.</title>
-    <?php include './partes/head.php'; ?>
-</head>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Artic Company S.A.S.</title>
+        <?php include './partes/head.php'; ?>
+    </head>
 
-<body id="pagina-principal">
-    <?php 
-        include './partes/header.php';
-        include './partes/servicios.php';
-        include './partes/portafolio.php';
-        include './partes/modalesportafolio.php';
-        include './partes/equipo.php';
-        include './partes/contactanos.php';
-        include './partes/footer.php';
-        include './partes/scripts.php';
-    ?>
-    
-</body>
-
-
+    <body id="pagina-principal">
+        <div itemscope itemtype="http://schema.org/Organization"></div>
+        <?php 
+            include './partes/header.php';
+            include './partes/servicios.php';
+            include './partes/portafolio.php';
+            include './partes/modalesportafolio.php';
+            include './partes/equipo.php';
+            include './partes/contactanos.php';
+            include './partes/footer.php';
+            include './partes/scripts.php';
+        ?>
+    </body>
 </html>
